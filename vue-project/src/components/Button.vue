e1b9b0a94d79390f7dafd3ed5f1be893916712e2
mhddhandhyptr <dhandhyminendy@gmail.com>
2024-03-31 15:06:46 +0700

    dhandhy

---
+++ vue-project/src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button class="btn">
+        submit
+    </button>
+</template>
+
+<!-- css vue3 -->
+<style scoped>
+.btn{
+    background-color: red;
+    color: white;
+    font-size: 28px;
+}
+</style>
---
+++ vue-project/src/components/TextHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      hello world
+     <buttonc/>
+     <buttonc/>
+     <TextH/>
+    </div>
+  </template>
+  -->
+  
+  <!-- vue 3 -->
+  <script setup>
+    import Buttonc from './components/Buttonc.vue'
+    import TextH from './components/Textheader.vue' 
+  </script>
